45f7c62f1b964c190c5105827923921f994850ca
이진섭 <ijinseob@mac-jinseoplee.local>
2023-06-10 19:54:54 +0900

    update action!

--- autoprefix-to-pr/index.ts
+++ autoprefix-to-pr/index.ts
@@ -1,11 +1,31 @@
+import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { Octokit } from '@octokit/rest';
 
-(() => {
-    if (!github.context.payload.pull_request) {
+(async () => {
+    const { owner, repo } = github.context.repo;
+    const pullRequest = github.context.payload.pull_request;
+
+    if (!pullRequest) {
         console.warn('Pull request does not exists');
+        return;
     }
 
+    const title = pullRequest.title as string;
+    const pullNumber = pullRequest.number;
     console.log('pr :: ', github.context.payload.pull_request);
+
+    const auth = core.getInput('repo-token', { required: true });
+    const octokit = new Octokit({
+        auth,
+    });
+
+    await octokit.rest.pulls.update({
+        owner,
+        repo,
+        pull_number: pullNumber,
+        title: `${title} [test-suffix]`,
+    });
 
     console.log('Succed executed');
 })();
