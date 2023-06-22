35286ef8af666275adb8572cb8f8f8311555a2f7
이 진섭 <79432498+lejinseop@users.noreply.github.com>
2023-06-23 00:31:42 +0900

    wjdk (#14)
    Co-authored-by: 이진섭 <ijinseob@mac-jinseoplee.local>

--- release-notegen/index.ts
+++ release-notegen/index.ts
@@ -71,8 +71,61 @@ const solution1 = async () => {
     console.log('Succed executed');
 }
 
-const solution2 = async () => {}
+const solution2 = async () => {
+    const { owner, repo } = github.context.repo;
+    const pullRequest = github.context.payload.pull_request;
+
+    console.log('context :: ', github.context);
+    const newTag = github.context.ref.replace('refs/tags/', '');
+    const newTagID: string = github.context.payload.after;
+
+    const auth = core.getInput('repo-token', { required: true });
+
+    const octokit = new Octokit({
+        auth,
+    });
+
+    const tags = await getTags({
+        filter: `${newTag.split('-')[0]}-*`,
+        orderBy: 'desc',
+    });
+
+    console.log('tags :: ', tags);
+
+    const latestTag = tags[1];
+    const latestTagID = await getTagID(latestTag);
+
+    console.log('latestTag :: ', latestTag);
+    console.log('latestTagID :: ', latestTagID);
+    console.log('newTag :: ', newTag);
+    console.log('newTagID :: ', newTagID);
+
+    const timeline = octokit.paginate.iterator(
+        octokit.repos.compareCommits.endpoint.merge({
+            owner,
+            repo,
+            base: latestTagID.substring(0, 7),
+            head: newTagID.substring(0, 7),
+        })
+    );
+
+    const commitItems = [];
+    for await (const response of timeline) {
+        const { data: compareCommits } = response;
+        console.log('compareCommits :: ', compareCommits);
+        // commitItems.push(...compareCommits.commits)
+    }
+
+    // await octokit.rest.pulls.update({
+    //     owner,
+    //     repo,
+    //     pull_number: pullNumber,
+    //     title: `${title} [test-suffix]`,
+    // });
+
+    console.log('Succed executed');
+}
 
 (async () => {
-    await solution1()
+    await solution2()
 })();
